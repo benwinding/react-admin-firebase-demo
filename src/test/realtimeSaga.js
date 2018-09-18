a98efedba38d1bc6835e2dff53541f30ae146c51
Ben Winding <ben.winding@gmail.com>
2018-09-18 11:33:14 +0930

    Realtime playing up on view change

--- src/test/realtimeSaga.js
+++ src/test/realtimeSaga.js
@@ -4,30 +4,31 @@ import {
   fb
 } from 'react-admin-firebase';
 
-const observeRequest = dataProvider => (type, resource, params) => {
+const observeRequest = (dataProvider, options) => (type, resource, params) => {
   // Filtering so that only posts are updated in real time
-  if (resource !== "posts") return;
+  console.log({options, resource});
+  if (resource !== 'posts') {
+    return;
+  }
+  if (options && options.observe && options.observe.includes(resource)) {
+    return;
+  }
 
   // Use your apollo client methods here or sockets or whatever else including the following very naive polling mechanism
   return {
     subscribe(observer) {
-      var r = fb.GetResource(resource);
-      var sub = r.realtimeObservable.subscribe(function (newList) {
+      const resourceObj = fb.GetResource(resource);
+      const sub = resourceObj.observable.subscribe(function () {
         dataProvider(type, resource, params)
-          .then(results => {
-            console.log({
-              results
-            });
-            observer.next(results);
-          }) // New data received, notify the observer
+          .then(results => observer.next(results)) // New data received, notify the observer
           .catch(error => observer.error(error)); // Ouch, an error occured, notify the observer
       });
 
       const subscription = {
         unsubscribe() {
-          sub.unsubscribe();
           // Notify the saga that we cleaned up everything
           observer.complete();
+          // sub.unsubscribe();
         }
       };
 
@@ -36,4 +37,6 @@ const observeRequest = dataProvider => (type, resource, params) => {
   };
 };
 
-export default dataProvider => realtimeSaga(observeRequest(dataProvider));
+export default (dataProvider) => { 
+  return realtimeSaga(observeRequest(dataProvider));
+}
