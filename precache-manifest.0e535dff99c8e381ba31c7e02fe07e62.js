5a4bc1b2c642dac4a939aabf39c53beed6ddee4b
Ben Winding <ben.winding@gmail.com>
2020-03-22 02:41:23 +1030

    Updates

---
+++ precache-manifest.0e535dff99c8e381ba31c7e02fe07e62.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff545e56f9da10452954d17fa04e8dea",
+    "url": "/react-admin-firebase-demo/index.html"
+  },
+  {
+    "revision": "f9283ad62489d993fadd",
+    "url": "/react-admin-firebase-demo/static/js/2.ad720510.chunk.js"
+  },
+  {
+    "revision": "02b5cd9c4206fad0371af382b7406740",
+    "url": "/react-admin-firebase-demo/static/js/2.ad720510.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b81c99538f05210f71d",
+    "url": "/react-admin-firebase-demo/static/js/main.69924c9a.chunk.js"
+  },
+  {
+    "revision": "e7ea42c2f4c57068997b",
+    "url": "/react-admin-firebase-demo/static/js/runtime-main.91039110.js"
+  }
+]);
