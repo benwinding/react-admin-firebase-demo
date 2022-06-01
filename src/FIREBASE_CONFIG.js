8348557fd8740e1c1f1444b5c570fb2b5777fb48
CyberTron <cybertron@CyberTrons-MacBook-Pro.local>
2022-06-01 19:27:32 +0500

    added fb config

---
+++ src/FIREBASE_CONFIG.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCJjA7JH6HsqEKTQjSxaMluzvMWqEk5Vo0",
+  authDomain: "visma-admin.firebaseapp.com",
+  projectId: "visma-admin",
+  storageBucket: "visma-admin.appspot.com",
+  messagingSenderId: "281395664465",
+  appId: "1:281395664465:web:7fcbfe8f937fcf317f8a70",
+  measurementId: "G-39PBJDF94F",
+};
